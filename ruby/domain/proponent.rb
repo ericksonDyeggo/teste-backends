5ea4ea0c7d7043072b34a7553e6ea866cd2a332b
Erickson Dyeggo <ericksondyeggo@gmail.com>
2020-11-08 23:40:44 -0300

    Adds validation for proponent's installments

--- ruby/domain/proponent.rb
+++ ruby/domain/proponent.rb
@@ -14,4 +14,21 @@ class Proponent
   def main?
     main
   end
+
+  def monthly_income_for?(installment)
+    monthly_income >= (installment * installment_multiplier)
+  end
+
+  private
+
+  def installment_multiplier
+    case age
+    when 18..23
+      4
+    when 24..50
+      3
+    else
+      2
+    end
+  end
 end
--- ruby/spec/domain/proponent_spec.rb
+++ ruby/spec/domain/proponent_spec.rb
@@ -23,4 +23,96 @@ describe Proponent do
       end
     end
   end
+
+  describe 'asking if the monthly income is valid for a installment' do
+    context "when the proponent's age is between 18 and 24 years" do
+      context "and the proponent's monthly income is at least 4 times the value of the installment" do
+        it 'returns true' do
+          age = 18
+          monthly_income = 4_000
+          installment = 1_000
+
+          subject = described_class.new(
+            id: nil, proposal_id: nil, name: nil, age: age, monthly_income: monthly_income, main: nil
+          ).monthly_income_for?(installment)
+
+          expect(subject).to be true
+        end
+      end
+
+      context "and the proponent's monthly income is less than 4 times the value of the installment" do
+        it 'returns false' do
+          age = 18
+          monthly_income = 3_999
+          installment = 1_000
+
+          subject = described_class.new(
+            id: nil, proposal_id: nil, name: nil, age: age, monthly_income: monthly_income, main: nil
+          ).monthly_income_for?(installment)
+
+          expect(subject).to be false
+        end
+      end
+    end
+
+    context "when the proponent's age is between 24 and 50 years" do
+      context "and the proponent's monthly income is at least 3 times the value of the installment" do
+        it 'returns true' do
+          age = 24
+          monthly_income = 3_000
+          installment = 1_000
+
+          subject = described_class.new(
+            id: nil, proposal_id: nil, name: nil, age: age, monthly_income: monthly_income, main: nil
+          ).monthly_income_for?(installment)
+
+          expect(subject).to be true
+        end
+      end
+
+      context "and the proponent's monthly income is less than 3 times the value of the installment" do
+        it 'returns false' do
+          age = 24
+          monthly_income = 2_999
+          installment = 1_000
+
+          subject = described_class.new(
+            id: nil, proposal_id: nil, name: nil, age: age, monthly_income: monthly_income, main: nil
+          ).monthly_income_for?(installment)
+
+          expect(subject).to be false
+        end
+      end
+    end
+
+    context "when the proponent's age is over 50 years" do
+      context "and the proponent's monthly income is at least 2 times the value of the installment" do
+        it 'returns true' do
+          age = 51
+          monthly_income = 2_000
+          installment = 1_000
+
+          subject = described_class.new(
+            id: nil, proposal_id: nil, name: nil, age: age, monthly_income: monthly_income, main: nil
+          ).monthly_income_for?(installment)
+
+          expect(subject).to be true
+        end
+      end
+
+      context "and the proponent's monthly income is less than 3 times the value of the installment" do
+        it 'returns false' do
+          age = 51
+          monthly_income = 1_999
+          installment = 1_000
+
+          subject = described_class.new(
+            id: nil, proposal_id: nil, name: nil, age: age, monthly_income: monthly_income, main: nil
+          ).monthly_income_for?(installment)
+
+          expect(subject).to be false
+        end
+      end
+    end
+  end
 end
